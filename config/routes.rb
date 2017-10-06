7e77588b15cca625be62d96b89cdb053d4414a9d
Vitaly Shlyaga <storkvist@storkvist.net>
2017-10-06 11:45:22 +0300

    small additionas to menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :issues, only: %i[index new create show]
+  resources :issues, only: %i[index new create show] do
+    collection do
+      get 'find'
+    end
+  end
 
   root to: 'static#home'
 end
