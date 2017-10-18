75653a57d2e67d6f352a934f61dcb5d702b730ee
Vitaly Shlyaga <storkvist@storkvist.net>
2017-10-18 11:35:29 +0300

    fix telegram bot

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,6 +1,6 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def start(data = nil, *)
     response = from ? "Привет, #{from['username']}!" : "Привет!"
-    respond_with :message, test: response
+    respond_with :message, text: response
   end
 end
